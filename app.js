c83c9903fbef6e4efdb05b0913d447933c796392
Sean Sobey SAHL <seans@sahomeloans.com>
2019-06-03 16:09:41 +0200

    Converted to google cloud func

--- app.js
+++ app.js
@@ -2,40 +2,13 @@
 'use strict';
 
 const express = require('express');
-const SpotifyWebApi = require('spotify-web-api-node');
 const envy = require('envy-optional');
+const { server } = require('./index');
 
 const config = envy(undefined, { checkPermissions: false });
 
 const app = express();
-const spotifyApi = new SpotifyWebApi({
-	clientId: config.spotifyClientId,
-	clientSecret: config.spotifyClientSecret,
-	redirectUri: config.spotifyRedirectUri,
-});
 
-app.use((request, response, next) => {
-	response.header("Access-Control-Allow-Origin", "*");
-	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
-
-app.post('/', (request, response) => {
-	const code = request.query.code;
-	if (!code) {
-		return response.status(403).send('Require code query parameter');
-	}
-	return spotifyApi.authorizationCodeGrant(code)
-		.then((data) => {
-			response.json(data.body);
-			console.debug('success', request, response);
-			return response;
-		})
-		.catch((error) => {
-			response.status(500).send(error);
-			console.error('error', error, request, response);
-			return response;
-		});
-});
+app.post('/', server);
 
 app.listen(config.port, () => console.log(`App listening on port ${config.port}`));
--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 //@ts-check
 'use strict';
 
+const SpotifyWebApi = require('spotify-web-api-node');
+const envy = require('envy-optional');
+
+const config = envy(undefined, { checkPermissions: false });
+
+const spotifyApi = new SpotifyWebApi({
+	clientId: config.spotifyClientId,
+	clientSecret: config.spotifyClientSecret,
+	redirectUri: config.spotifyRedirectUri,
+});
+
 /**
  * HTTP Cloud Function.
  * This function is exported by index.js, and is executed when
@@ -8,8 +19,30 @@
  *
  * @param {import('express').Request} request Cloud Function request context. More info: https://expressjs.com/en/api.html#req
  * @param {import('express').Response} response Cloud Function response context. More info: https://expressjs.com/en/api.html#res
- * @returns {void}
+ * @returns {import('express').Response}
  */
 exports.server = (request, response) => {
-	response.send('Hello World!');
+
+	response.header('Content-Type', 'application/json');
+	response.header('Access-Control-Allow-Origin', '*');
+	response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+	if (request.method == 'OPTIONS') {
+		return response.status(204).send('');
+	}
+	const code = request.query.code;
+	if (!code) {
+		return response.status(403).send('Require code query parameter');
+	}
+	return spotifyApi.authorizationCodeGrant(code)
+		.then((data) => {
+			response.json(data.body);
+			console.debug('success', request, response);
+			return response;
+		})
+		.catch((error) => {
+			response.status(500).send(error);
+			console.error('error', error, request, response);
+			return response;
+		});
 };
